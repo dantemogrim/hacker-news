fb927b8ab4160db5a0819fe0082059fa3061bb9f
mogrim-91 <66732473+mogrim-91@users.noreply.github.com>
2020-12-14 11:26:37 +0100

    CSS files in folder. index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Modern normalize CSS. -->
+    <link rel=“stylesheet” type=“text/css” href=“/css/modern-normalize.css”>
+    <!-- Modern normalize CSS. -->
+    <link rel=“stylesheet” type=“text/css” href=“/css/style.css”>
+    <title>Document</title>
+  </head>
+  <body></body>
+</html>
