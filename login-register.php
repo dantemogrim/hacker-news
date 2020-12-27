a42f7ec28bd74fba0f79b002f652464a2174e2c2
Dante Mogrim <66732473+mogrim-91@users.noreply.github.com>
2020-12-27 19:33:44 +0100

    basiclogin

---
+++ login-register.php
@@ -0,0 +1,25 @@
+<!-- Login form -->
+
+<form>
+
+	<section>
+	<label>E-mail</label>
+	<input type="email">
+	</section>
+
+<br>
+
+	<section>
+	<label>Passphrase</label>
+	<input type="password">
+    </section>	
+
+<br>
+
+<p>Forgot your password?</p>
+
+<button>Sign in</button>
+
+</form>
+
+<p>New? Click <b><u>here</u></b> to create an account.</p>
