6df06eee7fd344b77d17c6bb46112753cdf235c2
Dante Mogrim <66732473+mogrim-91@users.noreply.github.com>
2021-01-06 20:45:11 +0100

    header up and running bootstrap

--- views/header.php
+++ views/header.php
@@ -1,30 +1,29 @@
+<!-- Gets the backend up and running. -->
+<?php require __DIR__ . '/../app/autoload.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Title -->
+    <!-- Title -->
     <title><?php echo $config['title']; ?></title>
-        <!-- General CSS insertion. -->
-        <link rel="stylesheet" href="../assets/styles/app.css">
-        <!-- @font-face insertion. -->
-        <link rel="stylesheet" href="../assets/styles/fonts.css">
-        <!-- Hamburger menu CSS. -->
-        <link rel="stylesheet" href="../assets/styles/hamburger.css">
-        <!-- Media queries insertion. -->
-        <link rel="stylesheet" href="../assets/styles/media.css">
-        <!-- Modern-Normalize CSS insertion. -->
-        <link rel="stylesheet" href="../assets/styles/modern-normalize.css">
-        <!-- Favicon -->
-        <link rel="icon" type="images/png" href="../resources/media/icons/favicon.svg">
+    <!-- General CSS insertion. -->
+    <link rel="stylesheet" href="../assets/styles/app.css">
+    <!-- @font-face insertion. -->
+    <link rel="stylesheet" href="../assets/styles/fonts.css">
+    <!-- Hamburger menu CSS. -->
+    <link rel="stylesheet" href="../assets/styles/hamburger.css">
+    <!-- Media queries insertion. -->
+    <link rel="stylesheet" href="../assets/styles/media.css">
+    <!-- Modern-Normalize CSS insertion. -->
+    <link rel="stylesheet" href="../assets/styles/modern-normalize.css">
+    <!-- Favicon -->
+    <link rel="icon" type="images/png" href="../resources/media/icons/favicon.svg">
 </head>
 
 <body>
+    <?php require __DIR__ . '/navigation.php'; ?>
 
-<header>
-
-
-</header>
-
-    
+    <div class="container py-5">
