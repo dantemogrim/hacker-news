36ddc62fa52a9010ec8af44932c7fb0951229257
Dante Mogrim <66732473+mogrim-91@users.noreply.github.com>
2021-01-06 20:43:09 +0100

    Update functions.php

--- app/functions.php
+++ app/functions.php
@@ -7,3 +7,24 @@ function redirect(string $path)
     header("Location: ${path}");
     exit;
 }
+
+
+// ----------------- * REGISTER USER * ------------------ 
+// Asses that the data doesn't already exist. 
+
+// This built-in function will generate a powerful hash
+// $hash = password_hash($passphrase, PASSWORD_DEFAULT);
+
+
+
+
+
+
+// ----------------- * LOGIN USER * ------------------ 
+// Asses if user and password matches?
+
+// password_verify($passphrase, $hash);
+
+
+
+// ----------------- * LOGIN USER * ------------------ 
