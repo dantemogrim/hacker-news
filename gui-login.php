03d6083d10ec24c8fa68ccba26007dd9053250f1
Dante Mogrim <66732473+mogrim-91@users.noreply.github.com>
2021-01-14 14:18:01 +0100

    wip

--- gui-login.php
+++ gui-login.php
@@ -1,34 +1,34 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 
 <article>
-	<h1>Login</h1>
+    <h1>Login</h1>
 
-	<form action="app/users/login.php" method="post">
-		<div class="form-group">
-			<label for="username">Username:</label>
-			<input class="form-control" type="text" name="username" id="username" required>
-			<small class="form-text text-muted"></small>
-		</div><!-- /form-group -->
+    <form action="app/users/login.php" method="post">
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input class="form-control" type="text" name="username" id="username" required>
+            <small class="form-text text-muted"></small>
+        </div><!-- /form-group -->
 
-		<div class="form-group">
-			<span title="A passphrase is almost the same as a password. Instead of a just a single word however, we encourage our users to have at least four random words after each other. It's easy to remember and you reduce the risk of getting hacked. That way you're much safer off while surfing the web. If you have a password manager - even better!">
-				<label for="passphrase">Passphrase:</label></span>
-			<input class="form-control" type="password" name="passphrase" id="passphrase" required>
-			<small class="form-text text-muted"></small>
-		</div><!-- /form-group -->
+        <div class="form-group">
+            <span title="A passphrase is almost the same as a password. Instead of a just a single word however, we encourage our users to have at least four random words after each other. It's easy to remember and you reduce the risk of getting hacked. That way you're much safer off while surfing the web. If you have a password manager - even better!">
+                <label for="passphrase">Passphrase:</label></span>
+            <input class="form-control" type="password" name="passphrase" id="passphrase" required>
+            <small class="form-text text-muted"></small>
+        </div><!-- /form-group -->
 
-		<a href="#">
-			<p>Lost your passphrase? Click here.</p>
-		</a>
+        <a href="#">
+            <p>Lost your passphrase? Click here.</p>
+        </a>
 
-		<button type="submit" class="btn btn-primary">Login</button>
-	</form>
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
 </article>
 
 <br>
 
 <a href="/gui-register.php">
-	<p>New to this place? Click here to register.</p>
+    <p>New to this place? Click here to register.</p>
 </a>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
