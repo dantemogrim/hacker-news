9ae45d0284d563c1c08cb0c86966f84105adc312
Dante Mogrim <66732473+mogrim-91@users.noreply.github.com>
2021-01-20 11:52:50 +0100

    f

--- public/front/users/gui-profile.php
+++ public/front/users/gui-profile.php
@@ -1,25 +1,23 @@
 <?php require __DIR__ . '/../../header.php';
 
-
-?>
-
+if (!isset($_SESSION['loggedIn'])) :
+    redirect('/public/front/users/gui-login.php');
+endif; ?>
 
 
 <h2><i>Hello</i></h2>
 <h1><?php echo $_SESSION['loggedIn']['username']; ?> </h1>
 
 
-<?php
-$path = '/public/back/users/avatars/store/';
+<?php $path = '/public/back/users/avatars/store/'; ?>
 
-?>
 
 <img src="<?= $path . $_SESSION['loggedIn']['avatar']; ?> ">
 
 
 <br>
 
-<form action="/public/back/users/avatars/add-avatar.php" method="post" enctype="multipart/form-data">
+<form action="/public/back/users/avatars/edit-avatar.php" method="post" enctype="multipart/form-data">
     <div>
         <label for="avatar">Choose a PNG image to upload</label>
         <br>
