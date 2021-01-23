3392533c761e6c257d31c1585f5ce8614dbc4ae2
Dante Mogrim <66732473+mogrim-91@users.noreply.github.com>
2021-01-23 18:25:18 +0100

    wip smiles and edit/delete option on posts.

--- public/index.php
+++ public/index.php
@@ -27,8 +27,13 @@ $allPosts = fetchAllPosts($pdo);
     $postAuthor->bindParam(':user_id', $articlePost['user_id'], PDO::PARAM_STR);
     $postAuthor->execute();
     $singlePostAuthor = $postAuthor->fetch(PDO::FETCH_ASSOC);
-    ?>
 
+    $ownedBy = false;
+    if ($articlePost['user_id'] === $_SESSION['loggedIn']['id']) {
+        $ownedBy = true;
+    }
+
+    ?>
 
     <div class="card">
         <h5 class="card-header"></h5>
@@ -37,8 +42,21 @@ $allPosts = fetchAllPosts($pdo);
         <div class="card-body">
             <p class="card-text"><?= $articlePost['description']; ?></p>
             <span class="badge bg-warning text-dark">By: <?= $singlePostAuthor['username']; ?> @ <?= $articlePost['created_at']; ?></span>
-            <span class="badge bg-success"><img class="smiley" src="/public/resources/media/icons/smiley.png"> (amount) smiles</span>
+            <!--<span class="badge bg-success"><img class="smiley" src="/public/resources/media/icons/smiley.png"> (amount) smiles</span> -->
+            <!-- <img class="smiley" src="/public/resources/media/icons/smiley.png"> -->
+            <form action="/public/back/posts/smiles.php" method="post">
+                <button type="submit" aria-hidden="true">
+                    <img class="smiley" src="/public/resources/media/icons/smiley.png">
+                    <img class="smiley" src="/public/resources/media/icons/favicon.svg">
+                </button>
+            </form>
+
             <a href="/public/front/posts/gui-view-post.php?post_id=<?= $articlePost['id'] ?>">View Post</a>
+
+            <?php if ($ownedBy) : ?>
+                <p> Edit </p>
+                <p> Delete </p>
+            <?php endif; ?>
         </div>
     </div>
 
